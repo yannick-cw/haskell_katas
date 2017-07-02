458b59ea4e68a5f8926ad7436714d6abc2e23ead
yannick-cw <yannick.gladow@googlemail.com>
2017-07-02 18:16:04 +0200

    Updates anonymous functions specs.

--- test/Chapter7/AnonymousSpec.hs
+++ test/Chapter7/AnonymousSpec.hs
@@ -11,8 +11,18 @@ addOneIfOdd n =
   where
     f n = n + 1
 
+-- rewrite as lambda
+addFive :: Int -> Int -> Int
+addFive x y = (if x > y then y else x) + 5
+
+-- rewrite to not use lambda
+mflip :: (t2 -> t1 -> t) -> t1 -> t2 -> t
+mflip f = \x -> \y -> f y x
+
 spec :: Spec
 spec =
   describe "Anonymous function" $ do
     it "should add one for odd" $ addOneIfOdd 3 `shouldBe` 4
     it "should not add one for even" $ addOneIfOdd 4 `shouldBe` 4
+    it "should add five if x <= y" $ addFive 10 11 `shouldBe` 15
+    it "should apply f to x and y" $ mflip (-) 10 5 `shouldBe` -5

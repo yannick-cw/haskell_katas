efe63e105f35f875520ad2df6c6827cd51653ee6
Frank Boeddeker <frank.boeddeker@gmx.net>
2017-09-23 22:58:54 +0200

    Add tests for Chapter 2

---
+++ test/Chapter2/ExercisesSpec.hs
@@ -0,0 +1,35 @@
+module Chapter2.ExercisesSpec where
+
+import           Test.Hspec
+
+x = y ^ 2
+waxOn = x * 5
+y = z + 8
+z = 7
+
+triple x = x * 3
+
+waxOff x = triple x
+
+spec :: Spec
+spec =
+  describe "Exercises Chapter 2" $ do
+    it "should compile" $ "" `shouldBe` ""
+--    describe "Equivalent expressions" $ do
+--      it "1 should be true or false" $ (1 + 1) == 2 `shouldBe`  -- Enter True or False
+--      it "2 should be true or false" $ (10 ^ 2) == (10 + 9 * 10) `shouldBe`  -- Enter True or False
+--      it "3 should be true or false" $ (400 - 37) == ((-) 37 400) `shouldBe` -- Enter True or False
+--      -- Next test might throw an error.
+--      it "4 should be true or false" $ (100 `div` 3) == (100 / 3) `shouldBe` -- Enter True or False
+--      it "5 should be true or false" $ (2 * 5 + 18) == (2 * (5 + 18)) `shouldBe` -- Enter True or False
+
+
+--    describe "More fun with functions" $ do
+--      it "1a What do you expect" $ (1 + waxOn) `shouldBe`   -- Enter your expected value
+--      it "1b What do you expect" $ ((+10) waxOn) `shouldBe` -- Enter your expected value
+--      it "1c What do you expect" $ ((-) 15 waxOn) `shouldBe` -- Enter your expected value
+--      it "1d What do you expect" $ ((-) waxOn 15) `shouldBe` -- Enter your expected value
+--      it "What is the expected output" $ (triple waxOn) `shouldBe` -- Enter your expected value
+--      -- When the previous tests passed Rewrite waxOn as a function with a where clause an run tests again
+--      it "What is the expected output" $ (waxOff waxOn) `shouldBe` -- Enter your expected value
+
